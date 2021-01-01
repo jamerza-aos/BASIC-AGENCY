5bf6bd4da28b0636e127b1542c52d4dc011e9945
aos-jamerza <awsjamerza@gmail.com>
2021-01-01 14:33:08 +0100

    about

---
+++ public/assets/js/main.js
@@ -0,0 +1,35 @@
+function normalImg(index) {
+    index.style.height = "50vh";
+}
+function F1(img) {
+    img.style.zIndex = "5";
+    img.style.position = "absolute";
+}
+function Z1(img) {
+    img.style.zIndex = "3";
+    img.style.position = "absolute";
+}
+function F2(img) {
+    img.style.zIndex = "5";
+    img.style.position = "absolute";
+}
+function Z2(img) {
+    img.style.zIndex = "3";
+    img.style.position = "absolute";
+}
+function F3(img) {
+    img.style.zIndex = "6";
+    img.style.position = "absolute";
+}
+function Z3(img) {
+    img.style.zIndex = "2";
+    img.style.position = "absolute";
+}
+function F4(img) {
+    img.style.zIndex = 5;
+    img.style.position = "absolute";
+}
+function Z4(img) {
+    img.style.zIndex = 1;
+    img.style.position = "absolute";
+}
